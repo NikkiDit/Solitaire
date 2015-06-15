e0a97b6b4fa2db7cb6a48200b6f8cc8aef79368b
Adenike Olatunji <GloryOlatunji@Adenikes-MacBook-Pro.local>
2015-06-15 21:12:33 +0100

    updates

--- Solitaire/Solitaire/ViewController.m
+++ Solitaire/Solitaire/ViewController.m
@@ -47,7 +47,7 @@ const CGFloat FaceHeight = 8.0f;
 
 @implementation ViewController
 {
-    int xPosition, yPosition;
+    int xPosition, yPosition,moveCount,m;
     CGRect fCardOne;
     UIView *fCardTwo;
     CGRect fCardThree;
@@ -95,17 +95,61 @@ const CGFloat FaceHeight = 8.0f;
     
     
     [self cardStacks];
-    
-    //[self cardTableView:cardTable viewForRowAtIndex:m];
-    [self createCardDeck];
-    //  [self numberofView:cardTable];
+    [self getTimeLabel];
+    [self cardTableView:cardTable viewForRowAtIndex:m];
+ //   [self createCardDeck];
+    [self numberofView:cardTable];
 }
 
-- (void) createCardDeck{
+
+-(void) getTimeLabel{
+    
+    
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    
+    CGRect  timeRect = CGRectMake(140, 5, 30,45);
+    UILabel  *timeLabel = [[UILabel alloc] initWithFrame:timeRect];
+    
+    formatter.dateFormat= @"HH:mm";
+    timeLabel.text = [formatter stringFromDate:[NSDate date]];
+    timeLabel.textColor = [UIColor  blueColor];
+    timeLabel.adjustsFontSizeToFitWidth = YES;
+    timeLabel.clipsToBounds =YES;
+    timeLabel.textAlignment = NSTextAlignmentLeft;
+    CGAffineTransform labelSize = CGAffineTransformMakeScale(1.3, 1.3);
+    timeLabel.transform = labelSize;
+    [self.view addSubview:timeLabel];
+    
+}
+
+
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return nil;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 0;
+}
+
+
+- (NSInteger)numberofView:(UITableView *)cardTableView{
+    
+    
+    return [_cardArray count];
+    
+}
+
+
+
+- (UIView*)cardTableView:(UITableView *)cardView viewForRowAtIndex:(int)index{
+    
+    
     int labelCount = [_cardDeck count];
     int  b=2;
     xPosition= 2;
-    int m = 0;
     
     for (int i=1; i<=8; i++) {
         
@@ -203,14 +247,7 @@ const CGFloat FaceHeight = 8.0f;
             
             [_card addSubview:viewLabel3];
             [_card addSubview:viewLabel4];
-            /*
-             UITapGestureRecognizer *tapCard = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(cardWithTapGestureRecognizer:)];
-             
-             tapCard.numberOfTapsRequired = 1;
-             tapCard.numberOfTouchesRequired=1;
-             [_card setUserInteractionEnabled:YES];
-             [_card  addGestureRecognizer:tapCard];
-             */
+           
             [self.view addSubview:_card];
             [_cardArray addObject:_card];
             [_viewArray addObject:_card];
@@ -227,149 +264,16 @@ const CGFloat FaceHeight = 8.0f;
             if (m<(labelCount+1)) {
                 m =m+1;
             }else{
-                return;
+                return nil;
             }
         }
         
         xPosition=xPosition+4+CardWidth;
         b= b+1;
     }
-    
-    
-}
-
-
-
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     return nil;
-}
-
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 0;
-}
-
-
-- (NSInteger)numberofView:(UITableView *)cardTableView{
-    
-    
-    return [_cardArray count];
     
 }
-
-
-
-- (UIView*)cardTableView:(UITableView *)cardView viewForRowAtIndex:(int)index{
-    
-    
-    CGRect  viewRect = CGRectMake(xPosition, yPosition, CardWidth, CardHeight);
-    _card = [[UIView alloc] initWithFrame:viewRect];
-    [_card setContentMode:UIViewContentModeCenter];
-    _card.clipsToBounds =YES;
-    _card.backgroundColor = [UIColor whiteColor];
-    
-    UILabel *viewLabel3 = [[UILabel alloc] initWithFrame:CGRectMake(1,1,SuitWidth,SuitHeight)];
-    UILabel *viewLabel4 = [[UILabel alloc] initWithFrame:CGRectMake(CardWidth-21,CardHeight-42,SuitWidth,SuitHeight)];
-    
-    
-    [viewLabel3 setContentScaleFactor:0.2f];
-    viewLabel3.clipsToBounds = YES;
-    [viewLabel4  setContentScaleFactor:0.2f];
-    viewLabel4.clipsToBounds = YES;
-    
-    
-    _card.layer.cornerRadius = 5;
-    _card.layer.borderWidth = 1.0f;
-    _card.layer.borderColor =[UIColor grayColor].CGColor;
-    
-    
-    
-    
-    _viewCard =[_cardDeck objectAtIndex:random() % [_cardDeck count]];
-    
-    viewLabel3.text = _viewCard.description;
-    viewLabel4.text =_viewCard.description;
-    
-    
-    
-    if([viewLabel3.text containsString:@"♥" ] ||[viewLabel3.text containsString:@"♦"]||[viewLabel4.text containsString:@"♥" ] ||[viewLabel4.text containsString:@"♦"])
-    {
-        
-        viewLabel3.textColor = [UIColor redColor];
-        viewLabel4.textColor = [UIColor redColor];
-    }
-    
-    
-    viewLabel3.numberOfLines = 0;
-    [viewLabel3 sizeToFit];
-    viewLabel4.numberOfLines = 0;
-    [viewLabel4 sizeToFit];
-    
-    
-    CGAffineTransform labelSize = CGAffineTransformMakeScale(0.6, 0.6);
-    viewLabel3.transform = labelSize;
-    
-    // transform suit
-    CGAffineTransform xform = CGAffineTransformMakeRotation(M_PI);
-    
-    viewLabel4.transform = CGAffineTransformConcat(labelSize, xform);
-    
-    if([_viewCard.rank  isEqual: @"A"]){
-        _cardRank = 1;
-    }else if ([_viewCard.rank  isEqual: @"J"]){
-        _cardRank =11;
-    }else if([_viewCard.rank  isEqual: @"Q"]){
-        _cardRank =12;
-    }else if ([_viewCard.rank  isEqual: @"K"]){
-        _cardRank = 13;
-    }else{
-        _cardRank = [_viewCard.rank intValue] ;
-    }
-    
-    if ([viewLabel3.text containsString:@"♥"]){
-        _card.tag= 100+ _cardRank;
-    }
-    
-    if ([viewLabel3.text containsString:@"♠︎"]){
-        _card.tag =200+ _cardRank;
-    }
-    
-    if ([viewLabel3.text containsString:@"♦"]){
-        _card.tag = 300+_cardRank;
-    }
-    
-    
-    
-    
-    if ([viewLabel3.text containsString:@"♠"]){
-        _card.tag = 400+ _cardRank;
-    }
-    
-    [_card addSubview:viewLabel3];
-    [_card addSubview:viewLabel4];
-    
-    /*  UITapGestureRecognizer *tapCard = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(cardWithTapGestureRecognizer:)];
-     
-     tapCard.numberOfTapsRequired = 1;
-     tapCard.numberOfTouchesRequired=1;
-     [_card setUserInteractionEnabled:YES];
-     [_card  addGestureRecognizer:tapCard];
-     */
-    
-    [self.view addSubview:_card];
-    [_cardArray addObject:_card];
-    [_viewArray addObject:_card];
-    
-    
-    
-    [_cardDeck removeObject:_viewCard];
-    
-    
-    
-    
-    return nil;
-}
-
-
 
 
 
@@ -408,46 +312,40 @@ const CGFloat FaceHeight = 8.0f;
         
         if (_cardStack.tag == 100 ){
             stackLabel.text =@"♥";
-            stack_index = 0;
+            
             [stackLabel sizeToFit];
-            [_cardStackOne addObject:_cardStack ];
+           // [_cardStackOne addObject:_cardStack ];
             fCardOne =[ _cardStack frame];
             
         }
         if (_cardStack.tag ==200){
             stackLabel.text =@"♠︎";
             [stackLabel sizeToFit];
-            stack_index = 1;
             [_cardStackTwo addObject:_cardStack ];
             fCardTwo = _cardStack ;
             
-            NSLog(@"%f %f %ld", fCardTwo.frame.origin.x, fCardTwo.frame.origin.y, (long)fCardTwo.tag);
         }
         if (_cardStack.tag == 300){
             stackLabel.text =@"♦";
-            stack_index = 2;
+           
             [stackLabel sizeToFit];
             [_cardStackThree addObject:_cardStack ];
             fCardThree =[ _cardStack frame];
-            NSLog(@"%f %f", fCardThree.origin.x, fCardThree.origin.y);
-            
+         
         }
         
         
         if (_cardStack.tag ==400){
             stackLabel.text =@"♣︎";
             [stackLabel sizeToFit];
-            stack_index =3;
             [_cardStackFour addObject:_cardStack ];
             fCardFour =[ _cardStack frame];
-            NSLog(@"%f %f", fCardFour.origin.x, fCardFour.origin.y);
+         
         }
         
         
         [ _cardStack addSubview:stackLabel];
-        
-        
-        
+    
         [self.view addSubview:_cardStack];
         
         
@@ -460,68 +358,6 @@ const CGFloat FaceHeight = 8.0f;
     
 }
 
-/*
- -(void)cardWithTapGestureRecognizer:(UITapGestureRecognizer *)gestureRecognizer{
- 
- touchLocation = [gestureRecognizer locationInView:self.view];
- 
- [UIView transitionWithView:_card
- duration:0.1
- options:UIViewAnimationOptionAllowUserInteraction
- animations:^{
- //      NSLog(@"%d %f %f", _card.tag, _card.frame.origin.x, _card.frame.origin.y);
- _card.hidden = NO;
- self.view.hidden = NO;
- }
- completion:^(BOOL finished){
- // Save the old text and then swap the views.
- //  NSLog(@"%d %f %f", _card.tag, _card.frame.origin.x, _card.frame.origin.y);
- //                      UIView*    temp = _card ;
- 
- _card= [self hitTest: touchLocation withEvent:nil] ;
- 
- 
-[temp insertSubview:_cardStack aboveSubview:_card];
- 
- 
- }];
- 
- 
- 
- 
- 
- 
- }
- 
- 
- 
- - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
- {
- 
- UIView *hitView = [self.view hitTest:point withEvent:event];
- if ((hitView == self.view)||(hitView ==_cardshuffle)||(hitView ==_cardStack)) return nil;
- return hitView;
- }
- 
- 
- - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
- if (!_card.isUserInteractionEnabled || _card.isHidden || _card.alpha <= 0.01) {
- return nil;
- }
- if ([self pointInside:point withEvent:event]) {
- for (UIView *subview in [_card.subviews reverseObjectEnumerator]) {
- CGPoint convertedPoint = [subview convertPoint:point fromView:_card];
- UIView *hitTestView = [subview hitTest:convertedPoint withEvent:event];
- if (hitTestView) {
- return hitTestView;
- }
- }
- return _card;
- }
- return nil;
- }
- 
- */
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
     if (!_card.isUserInteractionEnabled || _card.isHidden || _card.alpha <= 0.01) {
         return nil;
@@ -562,8 +398,7 @@ const CGFloat FaceHeight = 8.0f;
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     CGPoint firstTouch = [touch locationInView:self.view];
-    
-    
+    [self getTimeLabel];
     
     for (UIView *view in _viewArray) {
         NSLog(@"%@", view);
@@ -579,7 +414,6 @@ const CGFloat FaceHeight = 8.0f;
     UITouch *touch = [touches anyObject];
     _currentTouch = [touch locationInView:self.view];
     _toMove.center= _currentTouch;
-    //  NSLog(@"%d", _toMove.tag);
     
 }
 
